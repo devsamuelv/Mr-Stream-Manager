43f593178f2677f241e92cde6b3ae5a1a17ecb64
DevSamuelV <realalloutmusic@gmail.com>
2020-06-11 12:32:29 -0400

    added all of the current features

--- MR_StreamManager.ts
+++ MR_StreamManager.ts
@@ -1,12 +1,16 @@
 import * as tmi from 'tmi.js';
+import { Client } from 'discord.js';
+import * as fs from 'fs';
 
 require('dotenv').config();
 
+const discord_bot = new Client();
 const discord = "https://discord.com/invite/TD8Dcsb";
 const github = "https://github.com/DevSamuelV";
-let project_name = "a Twitch Bot Called Mr Stream Manager";
-
-
+const project = {
+    github: "https://github.com/DevSamuelV/Mr-Stream-Manager",
+    name: "a Twitch Bot Called Mr Stream Manager aka me."
+}
 
 const client = tmi.Client({
     connection: {
@@ -31,16 +35,17 @@ console.log(client.connect());
 
 client.on('chat', (channel, user, message) => {
     switch(message) {
+        case "!help":
+            client.say(channel, "Available Commands !discord, !github, !dice, !project");
+            break;
+
         case "!discord":
             client.say(channel, `Here is the discord ${discord} ${user.username}`);
             break;
 
-        case '!ban':
-            if (!user.mod) { client.say(channel, `${user.username} you dont have that permission`); return; }
-
-            if (user.mod) {
-                
-            }
+        case '!dice':
+            const num = (Math.floor(Math.random() * 6) + 1);
+            client.say(channel, `${user.username} you rolled a ${num}`);
             break;
 
         case '!github':
@@ -48,7 +53,38 @@ client.on('chat', (channel, user, message) => {
             break;
 
         case "!project":
-            client.say(channel, `Samuel is working on ${project_name}`);
+            client.say(channel, `Samuel is working on ${project.name} also the project github ${project.github}`);
             break;
     }
+})
+
+client.once('chat', (channel, user, message) => {
+    if (message.includes("working on") || message.includes("is this")) {
+        client.say(channel, `Samuel is working on ${project.name} also the project github ${project.github}`);
+    }
+})
+
+client.on('subgift', (channel, username, streak, recipients, methods, userstate) => {
+    if (streak < 2) {
+        client.vip(channel, username);
+    }
+
+    client.say(channel, `${username} your crazy for the gifted subs`);
+})
+
+client.on('hosting', (channel, user, viewCount) => {
+    client.say(channel, `Thanks ${user} for hosting with ${viewCount} views.`);
+})
+
+client.on('ban', (channel, user, reason) => {
+    client.say(channel, `${user} was banned`);
+})
+
+client.on('cheer', (channel, userState, message) => {
+    if (Number(userState.bits) < 500) {
+        // @ts-ignore
+        client.vip(channel, userState.username);
+    }
+
+    client.say(channel, `Thanks ${userState.username} for the ${userState.bits} bits!!`);
 })
---
+++ README.md
@@ -0,0 +1,14 @@
+# Mr Stream Manager
+
+- **Features To Add**
+    - [X] !discord for the discord server
+    - [X] !github to show my github
+    - [X] !ad to show an ad **I dont know what its for??**
+    - [X] !dice to roll a dice
+    - [X] add discord integration to alert views that the stream is live
+    - [X] and message checking for what are you working on
+    - [X] add cheer alerts 
+    - [X] add gift sub alerts
+
+- **Features To Add later**
+    - [ ] Multi Channel Support
