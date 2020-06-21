67bf39382a6a926de046177f46fc4fb0006d9bb2
DevSamuelV <realalloutmusic@gmail.com>
2020-06-21 01:48:07 -0400

    added join events

--- MR_StreamManager.ts
+++ MR_StreamManager.ts
@@ -18,10 +18,24 @@ const connected_channels = {
     CodedToGame: "codedtogame"
 }
 
+const channelInfo = {
+    discord: {
+        samueltheboi: "https://discord.gg/2kuC5zD",
+        codedtogame: "https://discord.gg/BE8nDXs",
+    },
+
+    steamProfiles: {
+        samueltheboi: "https://steamcommunity.com/profiles/76561199029182001/",
+        crew: "https://steamcommunity.com/profiles/76561199029182001/",
+    },
+
+    github: {
+        samueltheboi: "https://github.com/DevSamuelV"
+    }
+}
+
 const firestore = firebase.firestore();
-const discord_2 = "https://discord.gg/rpkp3Tc";
-const discord = "https://discord.com/invite/TD8Dcsb";
-const github = "https://github.com/DevSamuelV";
+
 const project = {
     github: "https://github.com/DevSamuelV/Mr-Stream-Manager",
     name: "a Twitch Bot Called Mr Stream Manager aka me."
@@ -48,88 +62,88 @@ const client = tmi.Client({
 
 console.log(client.connect());
 
+// * commands
 client.on('chat', (channel, user, message) => {
-    switch (channel) {
-        case connected_channels.CodedToGame:
-            // * for the coded to game stream
-            switch (message) {
-                case '!color':
-                    var colors = ["blue", "red", "green", "Firebrick", "DodgerBlue", "CadetBlue", "OrangeRed", "HotPink"];
 
-                    var color_num = (Math.floor(Math.random() * Math.max(8)));
+    if (channel.includes("codedtogame")) {
+        switch (message) {
+            case '!color':
+                var colors = ["blue", "red", "green", "Firebrick", "DodgerBlue", "CadetBlue", "OrangeRed", "HotPink"];
 
-                    client.color(colors[color_num]);
-                    client.say(channel, `Bot Color Changed to ${colors[color_num]}`);
-                    break;
+                var color_num = (Math.floor(Math.random() * Math.max(8)));
 
-                case '!game':
-                    client.say(channel, `Crew and Sam are Playing Minecraft`);
-                    break;
-
-                case "!discord":
-                    client.say(channel, `Here is the discord ${discord_2} ${user.username}`); 
-                    break;
-            }
-            break;
-
-        case connected_channels.SamuelTheBoi:
-            // * for samueltheboi stream
-            switch (message) {
-                case "!help":
-                    client.say(channel, "Available Commands !discord, !github, !dice, !project");
-                    break;
-
-                case "!discord":
-                    client.say(channel, `Here is the discord ${discord} ${user.username}`);
-                    break;
-
-                case '!dice':
-                    const num = (Math.floor(Math.random() * 6) + 1);
-                    client.say(channel, `${user.username} you rolled a ${num}`);
-                    break;
-
-                case '!github':
-                    client.say(channel, github);
-                    break;
-
-                case "!project":
-                    client.say(channel, `Samuel is working on ${project.name} also the project github ${project.github}`);
-                    break;
-
-                case '!color':
-                    var colors = ["blue", "red", "green", "Firebrick", "DodgerBlue", "CadetBlue", "OrangeRed", "HotPink"];
-
-                    var color_num = (Math.floor(Math.random() * Math.max(8)));
-
-                    client.color(colors[color_num]);
-                    client.say(channel, `Bot Color Changed to ${colors[color_num]}`);
-                    break;
-
-                case '!ban':
-                    if (user.mod === true) {
-                        var input = message.split(' ')[1];
-                        if (input.length < 2) return;
-                        client.say(channel, `User ${input} was banned`);
-                    }
-                    break;
-            }
-            break;
-
-        default:
-            console.log("Channel Error <" + channel + ">")
-            break;
+                client.color(colors[color_num]);
+                client.say(channel, `Bot Color Changed to ${colors[color_num]}`);
+                break;
+        }
     }
 
+    if (channel.includes("samueltheboi")) {
+        switch (message) {
+            case "!help":
+                client.say(channel, "Available Commands !discord, !github, !dice, !project");
+                break;
 
+            case "!discord":
+                client.say(channel, `Here is the discord ${channelInfo.discord.samueltheboi} ${user.username}`);
+                break;
+
+            case '!dice':
+                const num = (Math.floor(Math.random() * 6) + 1);
+                client.say(channel, `${user.username} you rolled a ${num}`);
+                break;
+
+            // case '!github':
+            //     client.say(channel, channelInfo.github.samueltheboi);
+            //     break;
+
+            case "!project":
+                client.say(channel, `Samuel is working on ${project.name} also the project github ${project.github}`);
+                break;
+
+            case '!color':
+                var colors = ["blue", "red", "green", "Firebrick", "DodgerBlue", "CadetBlue", "OrangeRed", "HotPink"];
+
+                var color_num = (Math.floor(Math.random() * Math.max(8)));
+
+                client.color(colors[color_num]);
+                client.say(channel, `Bot Color Changed to ${colors[color_num]}`);
+                break;
+
+            case '!ban':
+                var args = message.split(' ');
+                if (user.mod === true) {
+                    if (args[1].length == 0) return;
+                    client.ban(channel, args[1], args[2]);
+                    client.say(channel, `User ${args[1]} was banned`);
+                } else {
+                    client.say(channel, `${args[1]} your not moderator`);
+                }
+                break;
+        }
+    }
 })
 
-client.once('chat', (channel, user, message) => {
-    switch (channel) {
-        case connected_channels.SamuelTheBoi:
-            if (message.includes("working on") || message.includes("is this")) {
-                client.say(channel, `Samuel is working on ${project.name} also the project github ${project.github}`);
-            }
+client.on('join', (channel, username, isSelf) => {
+    switch (username) {
+        case 'fierypilot420':
+            client.say(channel, `twitchRaid PJSalt PJSalt Lookout ${username} has joined twitchRaid PJSalt PJSalt`);
             break;
+
+        case 'AlbertOverbuild':
+            client.say(channel, `twitchRaid PJSalt PJSalt Lookout ${username} has joined twitchRaid PJSalt PJSalt`);
+            break;
+    }
+})
+
+// user chatting
+client.once('chat', (channel, user, message) => {
+    if (message.includes("working on") || message.includes("is this") || message.includes("whats this") || message.includes("how does it work")) {
+        client.say(channel, `Samuel is working on ${project.name} also the project github ${project.github}`);
+    }
+
+    if (message.toLowerCase().includes("hello")) {
+        client.say(channel, `Hello ${user.username} welcome to the stream KappaPride`);
     }
 })
 
@@ -155,7 +169,7 @@ client.on('hosting', (channel, user, viewCount) => {
     client.say(channel, `Thanks ${user} for hosting with ${viewCount} views.`);
 })
 
-client.on('ban', (channel, user, reason) => {
+client.once('ban', (channel, user, reason) => {
     client.say(channel, `${user} was banned`);
 })
 
@@ -167,14 +181,14 @@ client.on('cheer', (channel, userState, message) => {
 
     client.say(channel, `Thanks ${userState.username} for the ${userState.bits} bits!!`);
 
-    var data = {
-        username: userState.username,
-        message: message
-    }
+    // var data = {
+    //     username: userState.username,
+    //     message: message
+    // }
 
-    firestore.collection('subgift').add(data).catch((err) => {
-        console.error(err);
-    })
+    // firestore.collection('subgift').add(data).catch((err) => {
+    //     console.error(err);
+    // })
 })
 
 client.on('raided', (channel, username, viewCount) => {
@@ -182,3 +196,7 @@ client.on('raided', (channel, username, viewCount) => {
 
     client.say(channel, `${username} your crazy for raiding with ${viewCount} viewers.`);
 })
+
+function parseChannelName(channelName: string) {
+    return channelName.split("#")[1];
+}
